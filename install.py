741d2075c66a0338c67e913399a64c9128b73c17
Alex Genovese <423745+alexgenovese@users.noreply.github.com>
2024-06-17 16:20:05 +0200

    Update install.py

--- install.py
+++ install.py
@@ -2,11 +2,15 @@ import sys
 import os.path
 import subprocess
 from huggingface_hub import snapshot_download
-from folder_paths import folder_names_and_paths
+from folder_paths import folder_names_and_paths 
 
 CUSTOM_NODES_PATH = os.path.dirname(os.path.abspath(__file__))
 # WEIGHTS_PATH = os.path.join(CUSTOM_NODES_PATH, "models")
-WEIGHTS_PATH = folder_names_and_paths['checkpoints']['IDM_VTON']
+WEIGHTS_PATH = folder_names_and_paths['checkpoints'][0][0]
+if not os.path.exists(WEIGHTS_PATH + "/IDM_VTON"): 
+    os.makedirs(str(WEIGHTS_PATH) + "/IDM_VTON")
+
+WEIGHTS_PATH = WEIGHTS_PATH + "/IDM_VTON"
 HF_REPO_ID = "yisol/IDM-VTON"
 
 
