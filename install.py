26b0e12bbfe5392454d832381a7eef92b25792ba
Alex <423745+alexgenovese@users.noreply.github.com>
2024-07-27 16:52:55 +0200

    Update install.py

--- install.py
+++ install.py
@@ -3,9 +3,12 @@ import os.path
 import subprocess
 from huggingface_hub import snapshot_download
 
-CUSTOM_NODES_PATH = os.path.dirname(os.path.abspath(__file__))
-# WEIGHTS_PATH = os.path.join(CUSTOM_NODES_PATH, "models")
-WEIGHTS_PATH = "/comfyui/models/checkpoints/IDM_VTON"
+# it goes to root comfy folder
+sys.path.append('..')
+
+COMFYUI_ROOT = os.path.dirname(os.path.abspath(__file__))
+WEIGHTS_PATH = os.path.join(COMFYUI_ROOT, "models", "checkpoints", "IDM_VTON")
+# WEIGHTS_PATH = "/comfyui/models/checkpoints/IDM_VTON"
 download = False
 
 if not os.path.exists(WEIGHTS_PATH): 
