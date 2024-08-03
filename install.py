5a9225768d3613262393b309b0e3dba563a21965
Alex Genovese <423745+alexgenovese@users.noreply.github.com>
2024-08-03 23:37:13 +0200

    moved weight folder into checkpoints

--- install.py
+++ install.py
@@ -3,19 +3,9 @@ import os.path
 import subprocess
 from huggingface_hub import snapshot_download
 
-# it goes to root comfy folder
-sys.path.append('..')
 
-COMFYUI_ROOT = os.path.dirname(os.path.abspath(__file__))
-WEIGHTS_PATH = os.path.join(COMFYUI_ROOT, "models", "checkpoints", "IDM_VTON")
-# WEIGHTS_PATH = "/comfyui/models/checkpoints/IDM_VTON"
-download = False
-
-if not os.path.exists(WEIGHTS_PATH): 
-    download = True
-    print(f"--- create folder Not Exists")
-    os.makedirs(WEIGHTS_PATH)
-
+CUSTOM_NODES_PATH = os.path.dirname(os.path.abspath(__file__))
+WEIGHTS_PATH = os.path.join(CUSTOM_NODES_PATH, "models", "IDM-VTON")
 HF_REPO_ID = "yisol/IDM-VTON"
 
 
@@ -32,9 +22,11 @@ def ensure_package():
 
 if __name__ == "__main__":
     ensure_package()
-    if download:
-        print(f"---------------Starting snapsnot_download {WEIGHTS_PATH}")
+
+    if not os.path.exists(WEIGHTS_PATH):
+        os.makedirs(WEIGHTS_PATH)
+        # downlaod weights
+        print("Downloading VTON weights")
         snapshot_download(repo_id=HF_REPO_ID, local_dir=WEIGHTS_PATH)
-        print(f"---------------End snapsnot_download")
-    else: 
-        print(f"{WEIGHTS_PATH} is present - no download need")
+        print("End downloaded VTON weights")
+    
