704d49f73d5200ecafdb15d345d754cb7bab30ff
Alex Genovese <423745+alexgenovese@users.noreply.github.com>
2024-08-05 09:57:20 +0200

    fix wrong path

--- src/preprocess/openpose/annotator/util.py
+++ src/preprocess/openpose/annotator/util.py
@@ -5,10 +5,10 @@ import cv2
 import os
 from pathlib import Path
 
-PROJECT_ROOT = Path(__file__).absolute().parents[3].absolute()
-
+# PROJECT_ROOT = Path(__file__).absolute().parents[3].absolute()
+import folder_paths
+PROJECT_ROOT = os.path.join(folder_paths.models_dir,"checkpoints", "IDM-VTON")
 annotator_ckpts_path = os.path.join(PROJECT_ROOT, 'ckpt/openpose/ckpts')
-# print(annotator_ckpts_path)
 
 def HWC3(x):
     assert x.dtype == np.uint8
